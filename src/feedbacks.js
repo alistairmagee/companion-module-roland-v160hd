0539a0c8fb6035b67ec50466396d5208f3bd920e
Alistair Magee <alistair.magee@gmail.com>
2025-11-20 12:12:56 +1100

    feedback giving error don't know why

--- src/feedbacks.js
+++ src/feedbacks.js
@@ -337,63 +337,63 @@ module.exports = {
 			},
 		}
 
-		feedbacks.keyOnAir = {
-			type: 'boolean',
-			name: 'Key is Selected on Bus',
-			description: 'Indicate if Key is Selected on Bus',
-			style: {
-				color: foregroundColor,
-				bgcolor: backgroundColorRed,
-			},
-			options: [
-				{
-					type: 'dropdown',
-					label: 'PnP/Key',
-					id: 'pinp',
-					default: '1B',
-					choices: [
-						{ id: '1B', label: 'PnP/Key 1' },
-						{ id: '1C', label: 'PnP/Key 2' },
-						{ id: '1D', label: 'PnP/Key 3' },
-						{ id: '1E', label: 'PnP/Key 4' },
-					],
-				},
-				{
-					type: 'dropdown',
-					label: 'Bus',
-					id: 'bus',
-					default: '00',
-					choices: [
-						{ id: '00', label: 'Program (PGM)' },
-						{ id: '01', label: 'Preview (PVW)' },
-					],
-				},
-				{
-					type: 'dropdown',
-					label: 'On/Off',
-					id: 'onoff',
-					default: '01',
-					choices: [
-						{ id: '00', label: 'Off' },
-						{ id: '01', label: 'On' },
-					],
-				},
-			],
-			callback: function (feedback, bank) {
-				let opt = feedback.options
-
-				let obj = self.DATA.find((obj) => obj.id == `data_${opt.key}${opt.bus}`)
-
-				if (obj) {
-					if (obj.value == opt.onoff) {
-						return true
-					}
-				}
-
-				return false
-			},
-		}
-
+//		feedbacks.keyOnAir = {
+//			type: 'boolean',
+//			name: 'Key is Selected on Bus',
+//			description: 'Indicate if Key is Selected on Bus',
+//			style: {
+//				color: foregroundColor,
+//				bgcolor: backgroundColorRed,
+//			},
+//			options: [
+//				{
+//					type: 'dropdown',
+//					label: 'PnP/Key',
+//					id: 'pinp',
+//					default: '1B',
+//					choices: [
+//						{ id: '1B', label: 'PnP/Key 1' },
+//						{ id: '1C', label: 'PnP/Key 2' },
+//						{ id: '1D', label: 'PnP/Key 3' },
+//						{ id: '1E', label: 'PnP/Key 4' },
+//					],
+//				},
+//				{
+//					type: 'dropdown',
+//					label: 'Bus',
+//					id: 'bus',
+//					default: '00',
+//					choices: [
+//						{ id: '00', label: 'Program (PGM)' },
+//						{ id: '01', label: 'Preview (PVW)' },
+//					],
+//				},
+//				{
+//					type: 'dropdown',
+//					label: 'On/Off',
+//					id: 'onoff',
+//					default: '01',
+//					choices: [
+//						{ id: '00', label: 'Off' },
+//						{ id: '01', label: 'On' },
+//					],
+//				},
+//			],
+//			callback: function (feedback, bank) {
+//				let opt = feedback.options
+//
+//				let obj = self.DATA.find((obj) => obj.id == `data_${opt.key}${opt.bus}`)
+//
+//				if (obj) {
+//					if (obj.value == opt.onoff) {
+//						return true
+//					}
+//				}
+//
+//				return false
+//			},
+//		}
+//
 		feedbacks.freeze = {
 			type: 'boolean',
 			name: 'Freeze State',
